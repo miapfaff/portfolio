c379f8ac5892906bb69cf586aa909b23d9397a66
Mia Pfaff <miapfaff@udel.edu>
2024-06-03 19:49:43 -0400

    project section is toggling

--- start/scripts.js
+++ start/scripts.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var projectsLink = document.getElementById('projects-link');
     var projectsSection = document.getElementById('projects');
     projectsLink.addEventListener('click', function (e) {
-        console.log('Projects link clicked');
         e.preventDefault(); // Prevent default anchor behavior
         projectsSection.classList.toggle('hidden');
     });
